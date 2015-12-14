4ada7b8e9a985fab1fdff5865d556ed6ac298cad
Asfa <asfa.rani@gmail.com>
2015-12-14 21:35:18 +0000

    Add exercise 3

---
+++ Basics/exercise3.rb
@@ -0,0 +1,7 @@
+movies = {"Jurassic World" => 2015, "Gone Girl" => 2014, "Frozen" => 2013, "Skyfall" => 2012}
+
+puts movies["Jurassic World"]
+puts movies["Gone Girl"]
+puts movies["Frozen"]
+puts movies["Skyfall"]
+
