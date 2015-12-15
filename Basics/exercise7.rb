30b01d190a9b56f26b7e5a5b67ec18aa34d6662a
Asfa <asfa.rani@gmail.com>
2015-12-15 03:11:26 +0000

    Add remainder or exercises for Basics chapter

---
+++ Basics/exercise7.rb
@@ -0,0 +1,6 @@
+
+    
+"The error message is saying that there is a missing closing curly bracket somewhere.  
+So there is an opening curly bracket but no corresponding closing curly bracket." 
+    
+    
