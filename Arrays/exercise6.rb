3707ba5b8ee8925139c510fcaca731955b0deb0c
Asfa <asfa.rani@gmail.com>
2015-12-17 19:45:12 +0000

    Add Arrays exercises

---
+++ Arrays/exercise6.rb
@@ -0,0 +1,2 @@
+#You have to access the index of item in the Array with an integer. You cannot access it
+# by its name. You have to type names[3] = "jody"
---
+++ Arrays/exercise7.rb
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5,6]
+arr_new = []
+
+arr.each { |x| arr_new << x + 2 }
+
+p arr
+p arr_new
